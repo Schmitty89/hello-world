c28af0010a79b25d6d706ec337463173f1b29f61
Schmity89 <mr.jaysmith2@hotmail.com>
2016-06-22 09:57:33 -0700

    projects

---
+++ garage.js
@@ -0,0 +1,51 @@
+var garage = {
+  cars: [{
+    make: 'suzuki',
+    model: 'side - kick',
+    color: 'red'
+  }, {
+    make: 'mazda',
+    model: 'miata',
+    color: 'black'
+  }, {
+    make: 'honda',
+    model: 'civic',
+    color: 'purple'
+  }, {
+    make: 'toyota',
+    model: 'prius',
+    color: 'gold'
+  }, {
+    make: 'volkswagen',
+    model: 'rabbit',
+    color: 'green'
+  }, {
+    make: 'ford',
+    model: 'F150',
+    color: 'orange'
+  }
+]
+};
+  garage.buildCar = function() {
+    var make = prompt('What make?');
+    var model = prompt('what model?');
+    var color = prompt('what color?');
+  var newCar = ({make: make, model: model, color: color});
+  this.cars.push(newCar);
+};
+
+var Garage = function (cars) {
+  this.cars = cars
+};
+
+var Car = function (make,model,color) {
+  this.make = make;
+  this.model = model;
+  this.color = color;
+
+}
+var prowler = new Car ('plymouth','prowler','heat');
+var modelT = new Car ('ford','model-T-pick-up','wood pannel');
+var falcon = new Car ('ford','falcon','aztec gold');
+
+var myGarage = new Garage ([prowler, modelT, falcon]);
---
+++ javascript-intro.js
@@ -0,0 +1,36 @@
+var student = [
+  {
+    id: "1",
+    name: "Aaron",
+    row: "1",
+    userMac: "true",
+  }, {
+    id: "2",
+    name: 'Mike',
+    row: "2",
+    userMac: "false",
+  }, {
+    id: "3",
+    name: "Markham",
+    row: "3",
+    userMac: "true",
+  }, {
+    id: "4",
+    name: "Blaze",
+    row: "2",
+    userMac: "true",
+  }, ],
+};
+
+console.log(school.students[2].name);
+
+function addStudent(id, name, row, usesMac) {
+  this.id = id;
+  this.name = name;
+  this.row = row;
+  this.usesMac = usesMac;
+}
+
+function addNum (num) {
+var = (num +5) *50  )
+}
